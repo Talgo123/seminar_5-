4abfcfec591224b37d0bfd5d903534a17d7379ff
talgat <15talgat@mail.ru>
2022-12-08 21:49:50 +0300

    done zadacha_2

---
+++ zadacha_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на отрицательные, и наоборот
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // от минуса 9 до 10 (10 не включается)
+    }
+    return arr;
+}
+
+int[] array = new int[6];
+
+array = FillArrayWithRandomNumbers(6, -9, 9);
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = -array[i];
+}
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+
