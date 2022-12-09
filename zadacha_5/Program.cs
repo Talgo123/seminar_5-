4ca05ef6b0b40ccc691404f010796a450b85b017
talgat <15talgat@mail.ru>
2022-12-09 14:30:33 +0300

    done zadacha_5

---
+++ zadacha_5/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1); // от минуса 9 до 10 (10 не включается)
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+int leftRang = 1;
+int rightRang = 10;
+int[]array = FillArrayWithRandomNumbers(size, leftRang, rightRang);
+
+int[]newArray = new int [size / 2 + size % 2];
+int i = 0;
+for (i = 0; i < size / 2; i++)
+{
+    newArray[i] = array[i] * array[array.Length -1 - i];
+}
+if (array.Length % 2 != 0)
+{
+    newArray[newArray.Length -1] += array[array.Length % 2 +1];
+}
+System.Console.WriteLine('[' + string.Join(",", array) + ']');
+System.Console.WriteLine('[' + string.Join(" ", newArray) + ']');
